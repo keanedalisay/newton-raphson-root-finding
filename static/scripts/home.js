d6622a849fc3e2ea372d5578a1c623321ec7b055
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-12-11 15:56:56 +0800

    Refactor error and exception handling on the front-end and back-end

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -15,59 +15,66 @@ newtonRaphsonForm.addEventListener('submit', async (event) => {
     tolerance: parseFloat(formData.get('tolerance'))
   }
 
-  try {
-    const response = await fetch(newtonRaphsonForm.action, {
-      method: newtonRaphsonForm.method,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
+  const response = await fetch(newtonRaphsonForm.action, {
+    method: newtonRaphsonForm.method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
+  const result = await response.json()
 
-    const result = await response.json()
+  resultsSection.innerHTML = '' // Clear previous results
 
-    resultsSection.innerHTML = '' // Clear previous results
-
+  if (result.status !== 200) {
     resultsSection.insertAdjacentHTML(
       'beforeend',
       `
-      <h2>Root-Finding Results</h2>
-      <p>With <b>${result.iterations} iterations</b>, the approximate root is <b>${result.root_approximation}</b>.</p>
-      <p>The function value at this root approximation is <b>${result.function_value_at_root}</b>.</p>
-      <p>The derivative value at this root approximation is <b>${result.derivative_value_at_root}</b>.</p>
+      <h2 class="error">Status Code: ${result.status}</h2>
+      <p class="error">${result.message}</p>
       <hr>
       `
     )
 
-    let tableHTML = `
-      <table border="1">
-        <tr>
-          <th>Iteration</th>
-          <th>\\(x_n\\)</th>
-          <th>\\(f(x_n)\\)</th>
-          <th>\\(f'(x_n)\\)</th>
-        </tr>
-    `
-    for (const [i, row] of Object.entries(result.iteration_table)) {
-      tableHTML += `
-        <tr>
-          <td>${parseInt(i)}</td>
-          <td>${row.x}</td>
-          <td>${row['f(x)']}</td>
-          <td>${row["f'(x)"]}</td>
-        </tr>`
-    }
-
-    tableHTML += '</table><hr>'
-
-    resultsSection.insertAdjacentHTML('beforeend', tableHTML)
-
     updateMathJax()
-  } catch (error) {
-    console.error('Error: ', error)
+
+    return
   }
+
+  resultsSection.insertAdjacentHTML(
+    'beforeend',
+    `
+    <h2>Root-Finding Results</h2>
+    <p>With <b>${result.iterations} iterations</b>, the approximate root is <b>${result.root_approximation}</b>.</p>
+    <p>The function value at this root approximation is <b>${result.function_value_at_root}</b>.</p>
+    <p>The derivative value at this root approximation is <b>${result.derivative_value_at_root}</b>.</p>
+    <hr>
+    `
+  )
+
+  let tableHTML = `
+    <table border="1">
+      <tr>
+        <th>Iteration</th>
+        <th>\\(x_n\\)</th>
+        <th>\\(f(x_n)\\)</th>
+        <th>\\(f'(x_n)\\)</th>
+      </tr>
+  `
+  for (const [i, row] of Object.entries(result.iteration_table)) {
+    tableHTML += `
+      <tr>
+        <td>${parseInt(i)}</td>
+        <td>${row.x}</td>
+        <td>${row['f(x)']}</td>
+        <td>${row["f'(x)"]}</td>
+      </tr>`
+  }
+
+  tableHTML += '</table><hr>'
+
+  resultsSection.insertAdjacentHTML('beforeend', tableHTML)
+
+  updateMathJax()
 })
